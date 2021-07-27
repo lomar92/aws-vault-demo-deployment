0037319eeed3a2654474290ff43b1e157c4ba893
Sasano63 <sasano@hashicorp.com>
2021-07-27 15:20:55 +0200

    https

--- modules/vaultserver/vault.sh
+++ modules/vaultserver/vault.sh
@@ -121,7 +121,7 @@ sudo systemctl start vault
 
 sudo cat << 'EOF' > /tmp/authn.sh
 set -v
-export VAULT_ADDR="http://127.0.0.1:8200"
+export VAULT_ADDR="https://127.0.0.1:8200"
 vault operator init -format=json > vault.txt
 cat vault.txt | jq -r .root_token > vaulttoken
 pwd
