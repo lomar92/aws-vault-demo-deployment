3122f57e5b30980c90a1a58b9aa6d30d2f271664
Sasano63 <sasano@hashicorp.com>
2021-08-04 11:44:21 +0200

    - tags

--- main.tf
+++ main.tf
@@ -16,11 +16,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-default_tags = {
-    environment = "aws"
-    name = "vault"
-  }
-  
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
