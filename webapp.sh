1b2ddfd134a36608cd74a3404fe40fe74d66d06c
Sasano63 <sasano@hashicorp.com>
2021-08-05 15:14:55 +0200

    minor changes client

--- webapp.sh
+++ webapp.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-yum update -y
-yum install httpd -y
+sudo yum update -y
+sudo yum install -y yum-utils
+sudo yum install -y jq
+
 cd /var/www/html
 echo "Hallo Welt" > index.html
 service httpd start
