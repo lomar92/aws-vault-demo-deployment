9337fcb728dfe3ffe466db98b029af5979804d25
Sasano63 <sasano@hashicorp.com>
2021-09-17 09:26:05 +0200

    leader certs

--- modules/vaultserver/vault.sh
+++ modules/vaultserver/vault.sh
@@ -55,6 +55,10 @@ storage "raft" {
    retry_join {
     auto_join = "provider=aws addr_type=public_v4 region=eu-central-1 tag_key=project tag_value=vault"
     leader_tls_servername = "${raft_node}.eu-central-1.compute.internal"
+    leader_ca_cert_file     = "/etc/ssl/certs/ca.crt"
+    leader_client_cert_file = "/etc/ssl/certs/fullchain.crt"
+    leader_client_key_file  = "/etc/ssl/certs/privkey.key"
+
   }
 }
 seal "awskms" {
