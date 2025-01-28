3fe12d4f9d82788615b042b4d7367e9ebb47a1d0
Sara Asano <sasano@hashicorp.com>
2025-01-28 14:15:57 +0100

    count index

--- modules/vaultserver/outputs.tf
+++ modules/vaultserver/outputs.tf
@@ -1,9 +1,9 @@
 output "public_ip_instance" {
-  value = aws_instance.vaultserver.public_ip
+  value = aws_instance.vaultserver[count.index].public_ip
 }
 
 output "instance_id" {
-  value = aws_instance.vaultserver.id
+  value = aws_instance.vaultserver[count.index].id
 }
 
 output "vault_license" {
@@ -11,9 +11,9 @@ output "vault_license" {
 }
 
 output "public_dns_instance" {
-  value = aws_instance.vaultserver.public_dns
+  value = aws_instance.vaultserver[count.index].public_dns
 }
 
 output "private_ip_instance" {
-  value = aws_instance.vaultserver.private_ip
+  value = aws_instance.vaultserver[count.index].private_ip
 }
