49e7510ee66912ada7534c668ede543729a9a844
Sara Asano <sasano@hashicorp.com>
2025-01-29 16:54:57 +0100

    clean up

--- outputs.tf
+++ outputs.tf
@@ -10,9 +10,7 @@ output "public_ip3" {
   value = module.server3.public_ip_instance
 } */
 
-
-output "public_ips" {
-  value = {
-    for k in module.server : k => module.server[k].public_ip_instance
-  }
-}
+# The key expression produced an invalid result: string required.
+/* output "public_ips" {
+  value = { for k in module.server : k => module.server[k].public_ip_instance }
+} */
--- variables.tf
+++ variables.tf
@@ -1,34 +1,3 @@
-/* variable "node" {
-  type = map()
-  default = {
-    node1 = {
-      subnet  = aws_subnet.subnet_public[0].id,
-      
-    },
-    node2 = {
-      subnet  = aws_subnet.subnet_public[0].id,
-      
-    },
-    node3 = {
-      subnet  = aws_subnet.subnet_public[1].id,
-      
-    },
-    node4 = {
-      subnet  = aws_subnet.subnet_public[1].id,
-      
-    },
-    node5 = {
-      subnet  = aws_subnet.subnet_public[2].id,
-      
-    },
-    node6 = {
-      subnet  = aws_subnet.subnet_public[2].id,
-      
-    },
-  }
-}
- */
-
 variable "cidr_block" {
   type = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
@@ -90,11 +59,3 @@ variable "account_id" {
   description = "AWS account ID"
 }
 
-variable "username" {
-  default = "admin"
-}
-
-variable "dbpassword" {
-  default = "supergeheim"
-  sensitive = true
-}
